8891b0f5c1f574449159cc6fc9c726a804c3265d
unknown <you@example.com>
2023-12-15 00:51:54 +0300

    added main

--- project_fit/main/urls.py
+++ project_fit/main/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
-#внутри в этом файле мы сможешь отслеживать различные url адреса
+
+# внутри в этом файле мы сможешь отслеживать различные url адреса
 
 urlpatterns = [
-#панель администратора приложения (по умолчанию)
-    path('', views.index),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  # панель администратора приложения (по умолчанию)
+                  path('', views.index),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
